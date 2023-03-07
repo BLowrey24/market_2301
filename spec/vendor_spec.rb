15c6cdd26ab929972dcc91e2216e4724dd3d74e4
Boston Lowrey <bostonlowrey@gmail.com>
2023-03-07 11:30:55 -0500

    add test: for #stock in the Vendor class spec

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Vendor do
 
   let(:vendor) { Vendor.new('Rocky Mountain Fresh') }
   let(:item1) { Item.new({ name: 'Peach', price: "$0.75" }) }
+  let(:item2) { Item.new({ name: 'Tomato', price: '$0.50' }) } 
 
   describe '#initialize' do
     it 'exists' do
@@ -20,5 +21,17 @@ RSpec.describe Vendor do
     it 'checks the stock for an item and returns the number of items there are in stock' do
       expect(vendor.check_stock(item1)).to eq(0)
     end
+  end
+  
+  describe '#stock' do
+  vendor.stock(item1, 30)
+  expect(vendor.inventory).to eq({item1 => 30})
+  expect(vendor.check_stock(item1)).to eq(30)
+  
+  vendor.stock(item1, 25)
+  expect(vendor.check_stock(item1)).to eq(55)
+  
+  vendor.stock(item2, 12)
+  expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
   end
 end
